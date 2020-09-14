dc03c5e8be36b2d9ce313a6f7a889678c9ab9310
Alejandro Perez Malagon <alepmalagon@gmail.com>
2020-09-14 14:13:53 -0400

    aaaa

--- bigquery.py
+++ bigquery.py
@@ -5,7 +5,7 @@ client = bigquery.Client()
 # Perform a query.
 QUERY = (
 	'SELECT * FROM bigquery-public-data.covid19_open_data.compatibility_view AS COVID19 '
-    'WHERE COVID19.country_region = "Cuba" AND COVID19.province_state LIKE "%Havana%" AND COVID19.confirmed IS NOT NULL LIMIT 1 '
+    'WHERE COVID19.country_region = "Cuba" AND COVID19.province_state LIKE "%Havana%" AND COVID19.confirmed IS NOT NULL '
     'LIMIT 1')
 query_job = client.query(QUERY)  # API request
 rows = query_job.result()  # Waits for query to finish
